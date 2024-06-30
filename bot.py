02495e04ae8ca96f51d398a225028bbb7444cdf1
Titan_Cinemas <titancinemasindia@gmail.com>
2024-06-30 19:22:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -402,7 +402,7 @@ async def pm_media(bot, message):
             await bot.copy_message(
                 chat_id=owner_id,
                 from_chat_id=message.chat.id,
-                message_id=message.message_id,
+                message_id=message.message.id,
                 caption=IF_CONTENT.format(reference_id, info.first_name),
             )
     
