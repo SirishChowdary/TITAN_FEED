ce7722b9f0d739416f3dc8dee056a0045e7d82d2
Titan_Cinemas <titancinemasindia@gmail.com>
2024-07-01 13:10:04 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,11 +1,10 @@
 import os
 class Config(object):
-
     API_ID = int(os.environ.get("API_ID", "21821499"))
     API_HASH = str(os.environ.get("API_HASH", "31eda964c848701b76931b1a5446f301"))
     BOT_TOKEN = str(os.environ.get("BOT_TOKEN", "7145814610:AAHR7B5ql4-regRAPtRl5ebCeekMscaCRq8"))
     OWNER_ID = int(os.environ.get("OWNER_ID", "7158245271"))
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "6405622540 6529179563").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "6405622540 6529179563 7158245271").split())
     START = str(os.environ.get("START_TEXT", "wassup testing"))
     HELP = str(os.environ.get("HELP_TEXT", "help test"))
     DONATE = str(os.environ.get("DONATE_TEXT", "donation test"))
